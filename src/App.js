1b7f30d3cd032f90f44fdc594454e4161b13aaa1
Vaishali Umrikar <vumrikar@yahoo.com>
2018-04-11 07:30:32 -0500

    Assignment 3 Album fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Link } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
-import Albums from './components/Albums';
+import Album from './components/Album';
 
 
 class App extends Component {
@@ -20,7 +20,7 @@ class App extends Component {
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
-          <Route path="/albums" component={Albums} />
+          <Route path="/album" component={Album} />
         </main>
       </div>
     );
--- src/components/Albums.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Albums = () => (
-  <section className="albums">
-    Albums will go here
-  </section>
-);
-
-export default Albums;
