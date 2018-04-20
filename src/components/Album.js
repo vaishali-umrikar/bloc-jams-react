8f4e8bb572965a05125c5d78efea25dcd2db7a0f
Vaishali Umrikar <vumrikar@yahoo.com>
2018-04-20 06:31:14 -0500

    Assignment 8 player-bar: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -54,7 +54,7 @@ class Album extends Component {
 
     handleNextClick() {
       const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
-      const lastIndex= this.state.album.songs.length(song => this.state.currentSong === song) - 1;
+      const lastIndex= this.state.album.songs.length - 1;
       const newIndex = Math.min(lastIndex, currentIndex + 1);
       const newSong = this.state.album.songs[newIndex];
       this.setSong(newSong);
